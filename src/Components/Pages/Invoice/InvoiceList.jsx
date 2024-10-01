3fef20ef209c7fbb48c9b4da2a23c5b1dcdc5312
smansayeed <786saadman@gmail.com>
2024-10-01 23:46:08 +0600

    added preview button

--- src/Components/Pages/Invoice/InvoiceList.jsx
+++ src/Components/Pages/Invoice/InvoiceList.jsx
@@ -93,7 +93,7 @@ export default function InvoiceList() {
                       <button className="text-red-100 bg-red-500 rounded px-2 py-1 font-semibold" onClick={() => handleDelete(invoice.id)}>
                         Delete
                       </button>
-                      <Link to={routes.adminViewInvoice.link(invoice.id)}>View</Link>
+                      <Link className="text-blue-100 bg-blue-500 rounded px-2 py-1 font-semibold" to={routes.adminViewInvoice.link(invoice.id)}>View</Link>
                     </td>
                   </tr>
                 ))
